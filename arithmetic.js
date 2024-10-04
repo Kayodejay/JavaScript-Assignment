6e4113c57f854277149582e4f653b7cbe2baa8b3
Kayodejay <kayodeerioluwa465@gmail.com>
2024-10-04 12:22:33 -1100

    Initial commit

---
+++ arithmetic.js
@@ -0,0 +1,68 @@
+//ADDITION
+
+var addText = "Adding 10 and 30"
+
+document.write(addText)
+document.write("<br>")
+
+var addX = 10,
+    addY = 30;
+
+document.write(addX + addY)
+document.write("<br>")
+
+
+//SUBTRACTION
+
+document.write("<br>")
+var subtractText = "subtract 15 from 35"
+
+document.write(subtractText)
+document.write("<br>")
+
+var subtractX = 35,
+    subtractY = 15;
+
+document.write(subtractX - subtractY)
+document.write("<br>")
+
+//MULTIPLICATION
+
+document.write("<br>")
+var multiplyText = "multiply 16 by 12"
+
+document.write(multiplyText)
+document.write("<br>")
+
+var multiplyX = 16,
+    multiplyY = 12;
+
+document.write(multiplyX * multiplyY)
+document.write("<br>")
+
+//DIVISION
+
+document.write("<br>")
+var divideText = "dividing 4048 by 2"
+
+document.write(divideText)
+document.write("<br>")
+
+var divideX = 4048,
+    divideY = 2;
+
+document.write(divideX / divideY)
+document.write("<br>")
+
+//EXPONENTIAL
+
+document.write("<br>")
+var exponentText = "Taking exponent of 10 to the power of 2"
+
+document.write(exponentText)
+document.write("<br>")
+
+var exponentX = 10,
+    exponentY = 2
+document.write(exponentX ** exponentY)
+document.write("<br>")
